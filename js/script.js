09f952ffe6ef62c4016bf65d6738623182c2325f
Salvador <20230690@ricaldone.edu.sv>
2025-06-06 10:52:30 -0600

    Se agrego la funcionalidad de eliminar

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ function MostrarDatos(datos){
                 <td>${integrante.correo}</td>
                 <td>
                     <button>Editar</button>
-                    <button>Eliminar</button>
+                    <button onClick="EliminarPersona(${integrante.id})">Eliminar</button>
                 </td>
             </tr>
         `;
@@ -91,7 +91,24 @@ document.getElementById("frmAgregar").addEventListener("submit", async e=> {
 
         //Recargar la tabla
         ObtenerIntegrantes();
+    }else{
+        //En caso de que la API devuelva un codigo diferente a 200-299
+        alert("El registro no pudo ser agregado")
     }
-
-
 })
+
+//Funcionalidad borrar registros
+
+async function EliminarPersona(id){
+    const confirmacion = confirm("¿Realmente deseas eliminar el registro?")
+
+    //Validamos si el usuario si escogio borrar
+    if(confirmacion){
+        await fetch(`${API_URL}/${id}`, {
+            method: "DELETE",
+        });
+
+        //Recargar la tabla despues de eliminar
+        ObtenerIntegrantes();
+    }
+}
